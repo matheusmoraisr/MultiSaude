e97ff6817a25c8975c750850bd54af118aac9a98
matheusmoraisr <matheus.moraisrr@gmail.com>
2026-06-04 12:48:35 -0300

    Vamos ver se agora vai

---
+++ arquivo.js
@@ -0,0 +1,10 @@
+const select = document.getElementById("main-select");
+
+
+function troqueiDeValor(event){
+    console.log(event)
+}
+
+select.addEventListener("click", function(event){
+    console.log(event)
+}
